d8998d36a775e4c751fa0991da68b75268b74146
olivier poussel <olivier.poussel@gmail.com>
2021-07-06 09:22:17 +0200

    init commit

---
+++ cesi-app-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd1518bab128552ae16c48d3a5fe3cbee'),
+    },
+  },
+});
---
+++ cesi-app-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dbf99e82-0e23-44e5-8b86-fe32ba89abf6'
+};
